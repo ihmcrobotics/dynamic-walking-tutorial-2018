856f960f756c1c8180e882f0fed124dbab6967fd
Sylvain Bertrand <sbertrand@ihmc.us>
2018-05-17 14:17:25 -0500

    Some comments.

--- robot-arm-one/src/main/java/us/ihmc/robotArmOne/RobotArmOneSimulation.java
+++ robot-arm-one/src/main/java/us/ihmc/robotArmOne/RobotArmOneSimulation.java
@@ -1,24 +1,29 @@
 package us.ihmc.robotArmOne;
 
 import us.ihmc.simulationconstructionset.SimulationConstructionSet;
-import us.ihmc.simulationconstructionset.SimulationConstructionSetParameters;
 
 public class RobotArmOneSimulation
 {
    public RobotArmOneSimulation()
    {
-      double controlDT = 5.0e-5;
-
+      // Create an instance of the robot arm.
       RobotArmOne robotArm = new RobotArmOne();
+      // Create an instance of the controller.
       RobotArmOneController robotArmController = new RobotArmOneController(robotArm);
+      // Make sure to initialize the controller.
       robotArmController.initialize();
+      // Attach the controller to the robot.
       robotArm.setController(robotArmController);
 
-      SimulationConstructionSetParameters parameters = new SimulationConstructionSetParameters();
-      parameters.setDataBufferSize((int) Math.pow(2, 16)); // => 65536
-      SimulationConstructionSet scs = new SimulationConstructionSet(robotArm, parameters);
-      scs.setFastSimulate(true, 15);
-      scs.setDT(controlDT, 10);
+      // Creating the simulation.
+      SimulationConstructionSet scs = new SimulationConstructionSet(robotArm);
+
+      scs.setSimulateNoFasterThanRealTime(true);
+      // 
+      scs.setDT(1.0e-4, 10);
+      // Defining the buffer size to ensure a minimum simulation duration before filling the graphs in the simulator.
+      scs.setMaxBufferSize(65536);
+      // Launch the simulator.
       scs.startOnAThread();
    }
 
